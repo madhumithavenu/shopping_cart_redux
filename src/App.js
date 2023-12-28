202a385d58048dcf4e3b28547c382db92780b587
Madhumitha <madhumithavenu03@gmail.com>
2023-12-28 11:26:50 +0530

    adding auth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Auth from './components/auth/Auth';
 
 function App() {
   return (
     <div className="App">
-      <h1>HEllo</h1>
+      <Auth />
+      {/* <Layout /> */}
     </div>
   );
 }
